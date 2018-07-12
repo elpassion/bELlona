c84e0e870af922a34eaa18a1d795107d876f569c
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2018-07-12 11:48:46 +0200

    Fetch menu from website and parse it

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
-var http = require('http');
-var DOMParser = require('xmldom').DOMParser;
-var parser = new DOMParser();
+const http = require('http');
+const DOMParser = require('xmldom').DOMParser;
+const parser = new DOMParser();
 
 function getMenu(callback) {
-  return http.get('http://www.bufetmago.dwb.pl', function(response) {
-    var body = '';
-    response.on('data', function(d) {
+  return getDOMResponse('http://www.bufetmago.dwb.pl', function (content) {
+    const rows = content.getElementsByTagName('table')[1].getElementsByTagName('tr');
+    let menu = [];
+    for (let i = 0; i < rows.length; i++) {
+      const item = objectFromRow(rows[i]);
+      if (item != null) {
+        menu.push(item);
+      }
+    }
+    callback(menu);
+  });
+}
+
+function getDOMResponse(url, callback) {
+  return http.get(url, function (response) {
+    let body = '';
+    response.on('data', function (d) {
       body += d;
     });
-    response.on('end', function() {
-      var content = parser.parseFromString(body, 'text/html');
-      console.log(body)
+    response.on('end', function () {
+      const content = parser.parseFromString(body, 'text/html');
+      callback(content)
     });
   });
 }
 
-getMenu(function(){});
+function objectFromRow(row) {
+  const cells = row.getElementsByTagName('td');
+  if (cells.length !== 2) return null;
+
+  return {
+    name: cells[0].textContent,
+    price: parseFloat(cells[1].textContent),
+  }
+}
+
+getMenu(function (menu) {
+  console.log(menu)
+});
